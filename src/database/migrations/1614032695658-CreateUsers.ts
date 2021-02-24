741c5d7d4c6cea15de9d1739a743238478999784
Gabriel <bielmarques922@gmail.com>
2021-02-23 22:52:48 -0300

    Parte 02 - Cadastro de Usuário - Aula 01 e 02

--- src/database/migrations/1614032695658-CreateUsers.ts
+++ src/database/migrations/1614032695658-CreateUsers.ts
@@ -1,28 +1,31 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1613848962622 implements MigrationInterface {
+export default class CreateUsers1614032695658 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-               name: 'appointments',
-               columns: [
+                name: 'users',
+                columns: [
                    {
                        name: 'id',
                        type: 'uuid',
                        isPrimary: true,
                        generationStrategy: 'uuid',
                        default: 'uuid_generate_v4()'
-                   },
-                   {
-                        name: 'provider',
-                        type: 'varchar',
-                        isNullable: false,
-                   },
+                    },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
-                        isNullable: false
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'created_at',
@@ -40,7 +43,7 @@ export default class CreateAppointments1613848962622 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 
 }
